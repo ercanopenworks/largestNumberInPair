46c3d2e14ee3fcd9e494da632073d3cb0337b1c0
ercanerdogan@gmail.com <ercanerdogan@gmail.com>
2021-10-26 02:42:06 +0300

    solved

---
+++ largestNumber/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace largestNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> arr = Console.ReadLine().TrimEnd().Split(',').ToList().Select(a => Convert.ToInt32(a)).ToList();
+            Console.WriteLine(solution(arr.ToArray()));
+        }
+
+        public static int solution(int[] A)
+        {
+            int res = 0;
+
+            HashSet<int> set = new HashSet<int>();
+            foreach (var item in A)
+            {
+                if (set.Contains(item * -1)){
+                    res = Math.Max(res, Math.Abs(item));
+                }
+                else
+                {
+                    set.Add(item);
+                }
+
+            }
+            return res;
+
+        }
+    }
+}
